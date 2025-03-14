b30b1188626b42834bd2bc3c8850f0d858edf5ef
^.^ <miri0.0mitra@gmail.com>
2025-03-14 14:01:46 +0330

    Main class

---
+++ Answers/40330112120/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the grid: ");
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        Game game = new Game(size);
+        gmae.start();
+
+    }
+}
